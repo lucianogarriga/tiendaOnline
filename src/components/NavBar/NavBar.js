9ed8face0e949c0dd10fe5cce8a8d200dcd6053c
Luciano Garriga <lucianomgarriga@gmail.com>
2022-02-02 15:39:50 -0300

    Elaborando el navBar de mi sitio con React

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    return(
+        <div className='NavBar'>
+            <span className='nav-logo'>Burguer Boom</span>
+            <div className={`nav-items ${isOpen && "open"}`}>
+                <a href='/home'>Home</a>
+                <a href='/about'>About</a>
+                <a href='/product'>Product</a>
+                <a href='/contact'>Contact</a>
+            </div>
+            <div className={`nav-toggle ${isOpen && "open"}`} 
+            onClick={() => setIsOpen(!isOpen)}>
+                <div className='bar'></div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
--- src/home.js
+++ src/home.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className='App'>
       <NavBar/>
+      <h1>Este es el home</h1>
     </div>
   );
 }
