a0c3eb8a52e18107d2d3220e626842dabe1c3ef5
Luciano Garriga <lucianomgarriga@gmail.com>
2022-02-04 20:25:12 -0300

    Importando NavbarComp desde NavBar.js

---
+++ src/components/Carrito/Carrito.js
@@ -0,0 +1,9 @@
+import logo from '../../assets/replace.png';
+
+export const Carrito = () =>{
+    return(
+        <div>
+            <img src={logo} alt="logo"></img>
+        </div>
+    )
+}
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import ReplaceImg from '../../assets/replace.png'
+import React, {Component} from 'react';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 
-function NavBar () {
+export default class NavbarComp extends Component {
+    render(){
+        return(
+            <div>
+                <Navbar bg="dark" variant="dark"
+                sticky='top' expand='lg' collapseOnSelect>
+                    <Navbar.Brand>
+                        <img src={ReplaceImg} className="ReplaceImg"/>
+                    </Navbar.Brand>
 
-    return(
-        <>
-            
-        </>
-    )
+                    <Navbar.Toggle />
+                    <Navbar.Collapse>
+                    <Nav>
+                        <NavDropdown title='Equipos'>
+                        <NavDropdown.Item href='equipos/iphone'>Iphone</NavDropdown.Item>
+                        <NavDropdown.Item href='equipos/samsung'>Samsung</NavDropdown.Item>
+                        <NavDropdown.Item href='equipos/motorola'>Motorola</NavDropdown.Item>
+                        <NavDropdown.Item href='equipos/xiaomi'>Xiaomi</NavDropdown.Item>
+                        <NavDropdown.Divider/>
+                        <NavDropdown.Item href='equipos/promo'>Promociones</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="redes">Redes</Nav.Link>
+                        <Nav.Link href="contact">Contacto</Nav.Link>
+                        <Nav.Link href="about">Carrito</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                    
+                </Navbar>
+            </div>
+        )
+    }
 }
-
-export default NavBar;
