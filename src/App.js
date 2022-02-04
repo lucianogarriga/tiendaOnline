d2d54188463058e66753d4a487f4a733b2fa5ec4
Luciano Garriga <lucianomgarriga@gmail.com>
2022-02-04 17:41:33 -0300

    Reforma NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import NavBar from './components/NavBar/NavBar';
+import logo from './assets/replace.png'
+import React from 'react';
 import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className='App'>
-      <NavBar/>
-    </div>
+    <>
+      <div className='App'> 
+        <Navbar bg="dark" variant="dark"
+        sticky='top' expand='lg' collapseOnSelect>
+          <Navbar.Brand>
+            <img src={logo} className="logo"/>
+          </Navbar.Brand>
+
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+          <Nav>
+            <NavDropdown title='Equipos'>
+              <NavDropdown.Item href='equipos/iphone'>Iphone</NavDropdown.Item>
+              <NavDropdown.Item href='equipos/samsung'>Samsung</NavDropdown.Item>
+              <NavDropdown.Item href='equipos/motorola'>Motorola</NavDropdown.Item>
+              <NavDropdown.Item href='equipos/xiaomi'>Xiaomi</NavDropdown.Item>
+              <NavDropdown.Divider/>
+              <NavDropdown.Item href='equipos/promo'>Promociones</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="redes">Redes</Nav.Link>
+            <Nav.Link href="contact">Contacto</Nav.Link>
+            <Nav.Link href="about">Carrito</Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+          
+        </Navbar>
+        <div className='content'>
+        <p>Bienvenido</p>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,25 +1,11 @@
-import React, { useState } from 'react';
-import './NavBar.css';
+import React from 'react';
 
 function NavBar () {
-//const NavBar = () => {
-
-    const [isOpen, setIsOpen] = useState(false)
 
     return(
-        <div className='NavBar'>
-            <span className='nav-logo'>Burguer Boom</span>
-            <div className={`nav-items ${isOpen && "open"}`}>
-                <a href='/home'>Home</a>
-                <a href='/about'>About</a>
-                <a href='/product'>Product</a>
-                <a href='/contact'>Contact</a>
-            </div>
-            <div className={`nav-toggle ${isOpen && "open"}`} 
-            onClick={() => setIsOpen(!isOpen)}>
-                <div className='bar'></div>
-            </div>
-        </div>
+        <>
+            
+        </>
     )
 }
 
