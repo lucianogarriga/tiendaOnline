e029bc82d3ec276223502c4d1f69f19b2e95663a
Luciano Garriga <lucianomgarriga@gmail.com>
2022-03-15 19:44:58 -0300

    Estilos cards

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,10 +5,10 @@ export const Item = ({item} ) =>{
 
     return(
         <div className='col-md-4'>
-            <div className="card mt-5">
+            <div className="card mt-5 align-items-center">
                 <img className="card-img-top" src={item.imageUrl} style={{width: 280}} alt="Imagen"></img> 
-                <div className="card-body">
-                    <h5 className="card-title">{item.title}</h5>
+                <div className="card-body ">
+                    <h5 className="card-title ">{item.title}</h5>
                     <p> ${item.price}</p>
                     <Link to={`/item/${item.id}`}>
                         <button className="btn btn-primary"> Ver mas </button>
