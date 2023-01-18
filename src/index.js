ba1c5b2df6f9f62e03df0464a840013bf9d87e18
Luciano Garriga <lucianomgarriga@gmail.com>
2023-01-18 17:16:26 -0300

    Mods in package.json & more for deploy

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import 'font-awesome/css/font-awesome.min.css'
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
